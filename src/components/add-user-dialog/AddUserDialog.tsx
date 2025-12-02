00ea3565b423c167af6a12bda80f1b4874ea4c64
sid <sid@eratos.com>
2025-12-02 11:57:14 +1100

    add users in form submission

--- src/components/add-user-dialog/AddUserDialog.tsx
+++ src/components/add-user-dialog/AddUserDialog.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Cross2Icon, CheckIcon } from '@radix-ui/react-icons';
 import { useForm, type SubmitHandler } from 'react-hook-form';
+import { v4 as uuidv4 } from 'uuid';
+import { clsx } from 'clsx';
+
 import { Button } from '../button/Button';
 import styles from './AddUserDialog.module.css';
 import { AddUserAvatar } from '../add-user-avatar/AddUserAvatar';
 import { SelectAvatarButton } from '../select-avatar-button/SelectAvatarButton';
 import { SelectAvatarMenu } from '../select-avatar-menu/SelectAvatarMenu';
+
+import { useUserRepository } from '../../repositories';
+import { type IValidationError, type IPropertyError } from '../../types/IValidationError';
 
 interface AddUserDialogProps {
   open: boolean;
@@ -21,32 +27,58 @@ type FormInput = {
 };
 
 export const AddUserDialog = ({ open, onOpenChange }: AddUserDialogProps) => {
-  const [openAvatarSelectMenu, setOpenAvatarSelectMenu] = React.useState(false);
-  const handleOpenAvatarSelecMenu = () => setOpenAvatarSelectMenu((prev) => !prev);
+  const userRepository = useUserRepository();
 
+  // track the open state of the avatar select menu
+  const [openAvatarSelectMenu, setOpenAvatarSelectMenu] = React.useState(false);
+  // handle the change of open state of avatar select menu
+  const handleOpenAvatarSelectMenuChange = () => setOpenAvatarSelectMenu((prev) => !prev);
+
+  // track selected avatar state
   const [selectedAvatar, setSelectedAvatar] = React.useState('');
-  const handleSelctedAvatar = (imagUrl: string) => setSelectedAvatar(imagUrl);
+  // handle the change of selected avatar
+  const handleSelctedAvatarChange = (imagUrl: string) => setSelectedAvatar(imagUrl);
+
+  // track form submission errors
+  const [submitError, setSubmitError] = React.useState<IPropertyError[]>([]);
 
   const {
     register,
     handleSubmit,
     setValue,
-    formState: { errors, isValid },
+    formState: { errors, isSubmitting },
   } = useForm<FormInput>({
     shouldFocusError: false,
   });
 
-  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+  // handle form submission
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    try {
+      // reset any submit errors at the start of a submission
+      setSubmitError([]);
 
-  console.log({ errors, isValid });
+      const id = uuidv4();
+      await userRepository.add({
+        id,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        age: data.age ? Number(data.age) : undefined,
+        profileImageUrl: data.profileImageUrl,
+      });
 
+      // close dialog on success
+      onOpenChange(false);
+    } catch (error) {
+      // set submit error on error
+      setSubmitError((error as IValidationError).propertyErrors);
+    }
+  };
+
+  // set the profile image in the form data once selected by user
+  // runs when selectedAvatar changes
   React.useEffect(() => {
     setValue('profileImageUrl', selectedAvatar);
   }, [selectedAvatar, setValue]);
-
-  // TODO: fix all colours, spacing to design tokens
-
-  // TODO: wire up user repository
 
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
@@ -66,9 +98,9 @@ export const AddUserDialog = ({ open, onOpenChange }: AddUserDialogProps) => {
             <div className={styles.AvatarSection}>
               <div className={styles.SelectAvatar}>
                 <AddUserAvatar imageUrl={selectedAvatar} />
-                {<SelectAvatarButton open={openAvatarSelectMenu} onClick={handleOpenAvatarSelecMenu} />}
+                {<SelectAvatarButton open={openAvatarSelectMenu} onClick={handleOpenAvatarSelectMenuChange} />}
               </div>
-              {openAvatarSelectMenu && <SelectAvatarMenu handleSelectAvatar={handleSelctedAvatar} />}
+              {openAvatarSelectMenu && <SelectAvatarMenu handleSelectAvatar={handleSelctedAvatarChange} />}
             </div>
             <form id="add-user-form" className={styles.Form} onSubmit={handleSubmit(onSubmit)}>
               <div className={styles.FormSection}>
@@ -78,16 +110,17 @@ export const AddUserDialog = ({ open, onOpenChange }: AddUserDialogProps) => {
                 <input
                   className={
                     errors.firstName
-                      ? [styles.FormInput, styles.FormFieldError].join(' ')
-                      : styles.FormInput
+                      ? clsx(styles.FormInput, styles.FormInputError)
+                      : isSubmitting
+                        ? clsx(styles.FormInputDisabled, styles.FormInput)
+                        : styles.FormInput
                   }
                   {...register('firstName', {
                     required: { value: true, message: 'Please enter a value' },
                   })}
+                  disabled={isSubmitting}
                 />
-                {errors.firstName && (
-                  <label className={styles.ErrorLabel}>{errors.firstName.message}</label>
-                )}
+                {errors.firstName && <label className={styles.ErrorLabel}>{errors.firstName.message}</label>}
               </div>
               <div className={styles.FormSection}>
                 <label className={styles.FormLabel}>
@@ -96,27 +129,47 @@ export const AddUserDialog = ({ open, onOpenChange }: AddUserDialogProps) => {
                 <input
                   className={
                     errors.lastName
-                      ? [styles.FormInput, styles.FormFieldError].join(' ')
-                      : styles.FormInput
+                      ? clsx(styles.FormInput, styles.FormInputError)
+                      : isSubmitting
+                        ? clsx(styles.FormInputDisabled, styles.FormInput)
+                        : styles.FormInput
                   }
                   {...register('lastName', { required: { value: true, message: 'Please enter a value' } })}
+                  disabled={isSubmitting}
                 />
-                {errors.lastName && (
-                  <label className={styles.ErrorLabel}>{errors.lastName.message}</label>
-                )}
+                {errors.lastName && <label className={styles.ErrorLabel}>{errors.lastName.message}</label>}
               </div>
               <div className={styles.FormSection}>
                 <label className={styles.FormLabel}>Age</label>
-                <input className={styles.FormInput} {...register('age')} />
+                <input
+                  className={isSubmitting ? clsx(styles.FormInputDisabled, styles.FormInput) : styles.FormInput}
+                  {...register('age')}
+                  type="number"
+                  disabled={isSubmitting}
+                />
               </div>
             </form>
+            {submitError.length > 0 && (
+              <div className={styles.FormSubmitError}>
+                {submitError.map((error, i) => (
+                  <label key={i} className={styles.FormSubmitErrorLabel}>
+                    {error.message}
+                  </label>
+                ))}
+              </div>
+            )}
           </div>
           <div className={styles.Footer}>
             <Dialog.Close asChild>
               <Button className={styles.CancelButton}>Cancel</Button>
             </Dialog.Close>
             <div style={{ flexGrow: 1 }} />
-            <Button className={styles.SubmitButton} form="add-user-form" type="submit">
+            <Button
+              className={isSubmitting ? clsx(styles.SubmitButtonDisabled, styles.SubmitButton) : styles.SubmitButton}
+              form="add-user-form"
+              type="submit"
+              disabled={isSubmitting}
+            >
               <CheckIcon /> Create
             </Button>
           </div>
